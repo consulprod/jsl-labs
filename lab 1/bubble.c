edc23e124d36ad3cfca0390db13b22f6d357f6b9
vagrant <vagrant@php-training>
2024-03-15 15:10:25 -0700

    Add structures

---
+++ lab 1/bubble.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+[] bubble_sort(char [], int);
+[] bubble_sort(char list[], int n) {
+  int c, d, t, p;
+  for (c = 0 ; c < n - 1; c++) {
+    p = 0;
+    for (d = 0; d < n - c - 1; d++) {
+      if (list[d] > list[d+1]) {
+        t = list[d];
+        list[d] = list[d+1];
+        list[d+1] = t;
+        p++;
+      }
+    }
+    if (p == 0) break;
+  }
+  return list;
+}
